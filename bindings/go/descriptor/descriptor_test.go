2b3a170e18ac2e27fdd73129780a12bed28c8622
Jakob Möller <jakob.moeller@sap.com>
2025-03-17 12:40:13 +0100

    chore(ci): introduce linting via golangci-lint (#12)
    * chore(ci): introduce linting
    
    * chore(ci): correct lint
    
    * chore(ci): correct lint
    
    * chore(ci): correct lint
    
    * chore(ci): correct lint
    
    * chore(ci): correct lint
    
    * chore(ci): correct lint
    
    * chore(ci): correct lint
    
    * chore(ci): correct lint
    
    * chore(ci): correct lint
    
    * chore(ci): correct lint
    
    * chore(ci): correct lint
    
    * chore(ci): correct lint
    
    * chore(ci): correct lint
    
    * chore(ci): use small runners for now
    
    * chore(ci): fixup lint config
    
    * chore(ci): filter jobs
    
    * chore(ci): filter jobs
    
    * chore(ci): filter jobs
    
    * chore(ci): filter jobs
    
    * chore(ci): filter jobs
    
    * chore(ci): filter jobs
    
    * chore(ci): filter jobs
    
    * chore(ci): filter jobs
    
    * chore(ci): filter jobs
    
    * chore(ci): filter jobs
    
    * chore(ci): filter jobs
    
    * chore(ci): filter jobs
    
    * chore(ci): filter jobs

--- bindings/go/descriptor/descriptor_test.go
+++ bindings/go/descriptor/descriptor_test.go
@@ -7,4 +7,5 @@ import (
 func TestPlaceholder(t *testing.T) {
 	// This is a placeholder test to ensure the package is included in the build.
 	// Add actual tests here.
+	// TODO: Remove, Change Test
 }
