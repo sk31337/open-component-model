7b5c2b973db8cedca28e18c8512935af10034dca
Jakob Möller <jakob.moeller@sap.com>
2025-03-21 06:38:07 +0100

    chore: first maintenance round of descriptor v2 (#31)
    * fix: use original k8s deep copy gen for core runtime
    
    This uses the original deep copy gen instead of the controller runtime version as well as copies over the deep copy behavior from unstructured so that we use raw and unstructured in the same way as kubernetes
    
    * chore: first maintenance round of descriptor v2
    
    implements deep copy on all relevant structs and enforces the same type guarantees in v2 as in old ocm.
    
    * chore: unify ToIdentity and smaller findings
    
    * test: fixup raw parsing and add tests

--- bindings/go/descriptor/v2/descriptor.go
+++ bindings/go/descriptor/v2/descriptor.go
@@ -1,8 +1,10 @@
 package descriptor
 
 import (
+	"errors"
 	"fmt"
 	"maps"
+	"time"
 
 	"ocm.software/open-component-model/bindings/go/runtime"
 )
@@ -16,6 +18,14 @@ const (
 	NoDigest = "NO-DIGEST"
 )
 
+// These constants describe identity attributes predefined by the
+// model used to identify elements (resources, sources and references)
+// in a component version.
+const (
+	IdentityAttributeName    = "name"
+	IdentityAttributeVersion = "version"
+)
+
 // Descriptor defines a schema specific descriptor of a component containing additionally embedded signatures
 // verifying the validity of the component.
 type Descriptor struct {
@@ -27,7 +37,7 @@ type Descriptor struct {
 	Signatures []Signature `json:"signatures,omitempty"`
 }
 
-func (d Descriptor) String() string {
+func (d *Descriptor) String() string {
 	base := d.Component.String()
 	if d.Meta.Version != "" {
 		base += fmt.Sprintf(" (schema version %s)", d.Meta.Version)
@@ -38,13 +48,11 @@ func (d Descriptor) String() string {
 // Component defines a named and versioned component containing dependencies such as sources, resources and
 // references pointing to further component versions.
 type Component struct {
-	ObjectMeta `json:",inline"`
-	// Labels defines an optional set of additional labels
-	// describing the object.
-	Labels []Label `json:"labels,omitempty"`
+	ComponentMeta `json:",inline"`
 	// RepositoryContexts defines the previous repositories of the component.
+	// See https://github.com/open-component-model/ocm-spec/blob/main/doc/01-model/03-elements-sub.md#repository-contexts
 	RepositoryContexts []runtime.Unstructured `json:"repositoryContexts,omitempty"`
-	// Provider described the component provider.
+	// Provider describes the provider type of component in the origin's context.
 	Provider string `json:"provider"`
 	// Resources defines all resources that are created by the component and by a third party.
 	Resources []Resource `json:"resources,omitempty"`
@@ -54,11 +62,25 @@ type Component struct {
 	References []Reference `json:"componentReferences,omitempty"`
 }
 
+// ResourceRelation describes whether the component is created by a third party or internally.
+type ResourceRelation string
+
+const (
+	// LocalRelation defines a internal relation
+	// which describes a internally maintained resource in the origin's context.
+	LocalRelation ResourceRelation = "local"
+	// ExternalRelation defines a external relation
+	// which describes a resource maintained by a third party vendor in the origin's context.
+	ExternalRelation ResourceRelation = "external"
+)
+
+// A Resource is a delivery artifact, intended for deployment into a runtime environment, or describing additional content,
+// relevant for a deployment mechanism.
+// For example, installation procedures or meta-model descriptions controlling orchestration and/or deployment mechanisms.
+// See https://github.com/open-component-model/ocm-spec/blob/main/doc/01-model/02-elements-toplevel.md#resources
+// +k8s:deepcopy-gen=true
 type Resource struct {
-	ObjectMeta `json:",inline"`
-	// ExtraIdentity is the identity of an object.
-	// An additional label with key "name" is not allowed.
-	ExtraIdentity map[string]string `json:"extraIdentity,omitempty"`
+	ElementMeta `json:",inline"`
 	// SourceRefs defines a list of source names.
 	// These entries reference the sources defined in the
 	// component.sources.
@@ -67,59 +89,49 @@ type Resource struct {
 	Type string `json:"type"`
 	// Relation describes the relation of the resource to the component.
 	// Can be a local or external resource.
-	Relation string `json:"relation"`
+	Relation ResourceRelation `json:"relation"`
 	// Access defines the type of access this resource requires.
-	Access runtime.Raw `json:"access"`
+	Access *runtime.Raw `json:"access"`
 	// Digest is the optional digest of the referenced resource.
 	Digest *Digest `json:"digest,omitempty"`
 	// Size of the resource blob.
 	Size int64 `json:"size,omitempty"`
 	// CreationTime of the resource.
-	CreationTime string `json:"creationTime,omitempty"`
+	CreationTime *Timestamp `json:"creationTime,omitempty"`
 }
 
-func (r Resource) GetIdentity() map[string]string {
-	m := maps.Clone(r.ExtraIdentity)
-	if m == nil {
-		m = make(map[string]string)
-	}
-	m["name"] = r.Name
-	return m
-}
-
+// A Source is an artifact which describes the sources that were used to generate one or more of the resources.
+// Source elements do not have specific additional formal attributes.
+// See https://github.com/open-component-model/ocm-spec/blob/main/doc/01-model/02-elements-toplevel.md#sources
+// +k8s:deepcopy-gen=true
 type Source struct {
-	ObjectMeta    `json:",inline"`
-	ExtraIdentity map[string]string `json:"extraIdentity,omitempty"`
-	Type          string            `json:"type"`
-	Access        runtime.Raw       `json:"access"`
-}
-
-func (r Source) GetIdentity() map[string]string {
-	m := maps.Clone(r.ExtraIdentity)
-	if m == nil {
-		m = make(map[string]string)
-	}
-	m["name"] = r.Name
-	return m
+	ElementMeta `json:",inline"`
+	Type        string       `json:"type"`
+	Access      *runtime.Raw `json:"access"`
 }
 
 // Reference describes the reference to another component in the registry.
+// A component version may refer to other component versions by adding a reference to the component version.
+//
+// The Open Component Model makes no assumptions about how content described by the model is finally deployed or used.
+// This is left to external tools.
+//
+// Tool specific deployment information is formally represented by other artifacts with an appropriate type.
+//
+// In addition to the common artifact information, a resource may optionally describe a reference to the source by specifying its artifact identity.
+//
+// See https://github.com/open-component-model/ocm-spec/blob/main/doc/02-processing/01-references.md#referencing
+// +k8s:deepcopy-gen=true
 type Reference struct {
-	// Name of the reference.
-	Name string `json:"name"`
-	// ExtraIdentity defines additional information.
-	ExtraIdentity map[string]string `json:"extraIdentity,omitempty"`
+	ElementMeta `json:",inline"`
 	// Component describes the remote name of the referenced object.
 	Component string `json:"componentName"`
-	// Version defines the version of the reference.
-	Version string `json:"version"`
 	// Digest of the reference.
 	Digest Digest `json:"digest,omitempty"`
-	// Labels provided for further identification and extra selection rules.
-	Labels []Label `json:"labels,omitempty"`
 }
 
 // SourceRef defines a reference to a source.
+// +k8s:deepcopy-gen=true
 type SourceRef struct {
 	// IdentitySelector provides selection means for sources.
 	IdentitySelector map[string]string `json:"identitySelector,omitempty"`
@@ -127,29 +139,97 @@ type SourceRef struct {
 	Labels []Label `json:"labels,omitempty"`
 }
 
+// Meta defines the metadata of the component descriptor.
+// +k8s:deepcopy-gen=true
 type Meta struct {
+	// Version is the schema version of the component descriptor.
 	Version string `json:"schemaVersion"`
 }
 
-// ObjectMeta defines an object with name and version containing labels.
+// ObjectMeta defines an object that is uniquely identified by its name and version.
+// Additionally the object can be defined by an optional set of labels.
+// It is an implementation of the Element Identity as per
+// https://github.com/open-component-model/ocm-spec/blob/main/doc/01-model/03-elements-sub.md#element-identity
+// +k8s:deepcopy-gen=true
 type ObjectMeta struct {
-	Name    string  `json:"name"`
-	Version string  `json:"version"`
-	Labels  []Label `json:"labels,omitempty"`
+	// Name is the context unique name of the object.
+	Name string `json:"name"`
+	// Version is the semver version of the object.
+	Version string `json:"version"`
+	// Labels defines an optional set of additional labels
+	// describing the object.
+	// +optional
+	Labels []Label `json:"labels,omitempty"`
 }
 
-func (o ObjectMeta) String() string {
-	base := o.Name
-	if o.Version != "" {
-		base += ":" + o.Version
+func (m *ObjectMeta) String() string {
+	base := m.Name
+	if m.Version != "" {
+		base += ":" + m.Version
 	}
-	if o.Labels != nil {
-		base += fmt.Sprintf(" (%v)", o.Labels)
+	if len(m.Labels) > 0 {
+		base += fmt.Sprintf("+labels(%v)", m.Labels)
 	}
 	return base
 }
 
+// ElementMeta defines an object with name and version containing labels.
+// +k8s:deepcopy-gen=true
+type ElementMeta struct {
+	ObjectMeta `json:",inline"`
+	// ExtraIdentity is the identity of an object.
+	// An additional label with key "name" is not allowed
+	ExtraIdentity runtime.Identity `json:"extraIdentity,omitempty"`
+}
+
+func (m *ElementMeta) String() string {
+	base := m.ObjectMeta.String()
+	if m.ExtraIdentity != nil {
+		base += fmt.Sprintf("+extraIdentity(%v)", m.ExtraIdentity)
+	}
+	return base
+}
+
+// ToIdentity returns the runtime.Identity equivalent of the ElementMeta.
+// It is used to create a unique identity for the object.
+func (m *ElementMeta) ToIdentity() runtime.Identity {
+	if m == nil {
+		return nil
+	}
+	mp := maps.Clone(m.ExtraIdentity)
+	if mp == nil {
+		mp = make(runtime.Identity, 2)
+	}
+	mp[IdentityAttributeName] = m.Name
+	mp[IdentityAttributeVersion] = m.Version
+	return mp
+}
+
+// ComponentMeta defines an object with name and version containing labels.
+// The ObjectMeta is used to contain the Component Identity as per
+// https://github.com/open-component-model/ocm-spec/blob/main/doc/01-model/02-elements-toplevel.md#component-identity
+// +k8s:deepcopy-gen=true
+type ComponentMeta struct {
+	ObjectMeta `json:",inline"`
+	// CreationTime is the creation time of the component version
+	CreationTime string `json:"creationTime,omitempty"`
+}
+
+// ToIdentity returns the runtime.Identity equivalent of the ElementMeta.
+// It is used to create a unique identity for the object.
+func (r *ComponentMeta) ToIdentity() runtime.Identity {
+	if r == nil {
+		return nil
+	}
+	m := make(runtime.Identity, 2)
+	m[IdentityAttributeName] = r.Name
+	m[IdentityAttributeVersion] = r.Version
+	return m
+}
+
 // Digest defines digest information such as hashing algorithm, normalization and the actual value.
+// See https://github.com/open-component-model/ocm-spec/blob/main/doc/01-model/03-elements-sub.md#digest-info
+// +k8s:deepcopy-gen=true
 type Digest struct {
 	HashAlgorithm          string `json:"hashAlgorithm"`
 	NormalisationAlgorithm string `json:"normalisationAlgorithm"`
@@ -157,23 +237,81 @@ type Digest struct {
 }
 
 // Signature contains a list of signatures for the component.
+// See https://github.com/open-component-model/ocm-spec/blob/main/doc/01-model/03-elements-sub.md#signatures
+// See https://github.com/open-component-model/ocm-spec/blob/main/doc/02-processing/02-signing.md
+// +k8s:deepcopy-gen=true
 type Signature struct {
 	Name      string        `json:"name"`
 	Digest    Digest        `json:"digest"`
-	Signature SignatureSpec `json:"signature"`
+	Signature SignatureInfo `json:"signature"`
 }
 
-// SignatureSpec defines details of a signature.
-type SignatureSpec struct {
+// SignatureInfo defines details of a signature.
+// See https://github.com/open-component-model/ocm-spec/blob/main/doc/01-model/03-elements-sub.md#signature-info
+// +k8s:deepcopy-gen=true
+type SignatureInfo struct {
 	Algorithm string `json:"algorithm"`
 	Value     string `json:"value"`
 	MediaType string `json:"mediaType"`
 	Issuer    string `json:"issuer,omitempty"`
 }
 
+// Label that can be set on various objects in the Open Component Model domain.
+// See https://github.com/open-component-model/ocm-spec/blob/main/doc/01-model/03-elements-sub.md#labels
+// +k8s:deepcopy-gen=true
 type Label struct {
 	Name  string `json:"name"`
 	Value string `json:"value"`
 	// Signing describes whether the label should be included into the signature
 	Signing bool `json:"signing,omitempty"`
+}
+
+// NewExcludeFromSignatureDigest returns the special digest notation to indicate the resource content should not be part of the signature.
+func NewExcludeFromSignatureDigest() *Digest {
+	return &Digest{
+		HashAlgorithm:          NoDigest,
+		NormalisationAlgorithm: ExcludeFromSignature,
+		Value:                  NoDigest,
+	}
+}
+
+// Timestamp is time rounded to seconds.
+// +k8s:deepcopy-gen=true
+type Timestamp struct {
+	Time `json:",inline"`
+}
+
+// MarshalJSON implements the json.Marshaler interface.
+// The time is a quoted string in RFC 3339 format, with sub-second precision added if present.
+func (t *Timestamp) MarshalJSON() ([]byte, error) {
+	if y := t.Year(); y < 0 || y >= 10000 {
+		// RFC 3339 is clear that years are 4 digits exactly.
+		// See golang.org/issue/4556#c15 for more discussion.
+		return nil, errors.New("Time.MarshalJSON: year outside of range [0,9999]")
+	}
+
+	b := make([]byte, 0, len(time.RFC3339)+2)
+	b = append(b, '"')
+	b = t.AppendFormat(b, time.RFC3339)
+	b = append(b, '"')
+	return b, nil
+}
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+// The time is expected to be a quoted string in RFC 3339 format.
+func (t *Timestamp) UnmarshalJSON(data []byte) error {
+	// Ignore null, like in the main JSON package.
+	if string(data) == "null" {
+		return nil
+	}
+
+	// Fractional seconds are handled implicitly by Parse.
+	tt, err := time.Parse(`"`+time.RFC3339+`"`, string(data))
+	if err != nil {
+		return err
+	}
+	*t = Timestamp{
+		NewTime(tt.UTC().Round(time.Second)),
+	}
+	return err
 }
--- bindings/go/descriptor/v2/descriptor_test.go
+++ bindings/go/descriptor/v2/descriptor_test.go
@@ -1,11 +1,16 @@
-package descriptor
+package descriptor_test
 
 import (
 	"encoding/json"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"sigs.k8s.io/yaml"
+
+	"ocm.software/open-component-model/bindings/go/descriptor"
+	"ocm.software/open-component-model/bindings/go/runtime"
 )
 
 const jsonData = `
@@ -149,20 +154,489 @@ signatures:
 `
 
 func TestDescriptor_JSON(t *testing.T) {
-	desc := &Descriptor{}
-	err := json.Unmarshal([]byte(jsonData), desc)
+	desc := descriptor.Descriptor{}
+	err := json.Unmarshal([]byte(jsonData), &desc)
 	assert.Nil(t, err)
+
+	assert.NotEmpty(t, desc.Component.Resources[0].ToIdentity())
+
 	descData, err := json.Marshal(desc)
 	assert.JSONEq(t, jsonData, string(descData))
 	assert.Nil(t, err)
 }
 
 func TestDescriptor_YAML(t *testing.T) {
-	desc := &Descriptor{}
+	desc := &descriptor.Descriptor{}
 	err := yaml.Unmarshal([]byte(yamlData), desc)
 	assert.Nil(t, err)
 	descData, err := yaml.Marshal(desc)
 	assert.YAMLEq(t, yamlData, string(descData))
 	assert.Nil(t, err)
 	_ = descData
+}
+
+func TestDescriptor_String(t *testing.T) {
+	// Setup
+	desc := descriptor.Descriptor{
+		Meta: descriptor.Meta{
+			Version: "v1",
+		},
+		Component: descriptor.Component{
+			ComponentMeta: descriptor.ComponentMeta{
+				ObjectMeta: descriptor.ObjectMeta{
+					Name:    "test-component",
+					Version: "1.0.0",
+				},
+			},
+		},
+	}
+
+	// Test
+	result := desc.String()
+
+	// Assert
+	expected := "test-component:1.0.0 (schema version v1)"
+	assert.Equal(t, expected, result)
+}
+
+func TestComponent_String(t *testing.T) {
+	// Setup
+	comp := descriptor.Component{
+		ComponentMeta: descriptor.ComponentMeta{
+			ObjectMeta: descriptor.ObjectMeta{
+				Name:    "test-component",
+				Version: "1.0.0",
+				Labels: []descriptor.Label{
+					{Name: "env", Value: "prod"},
+				},
+			},
+		},
+	}
+
+	// Test
+	result := comp.String()
+
+	// Assert
+	expected := "test-component:1.0.0+labels([{env prod false}])"
+	assert.Equal(t, expected, result)
+}
+
+func TestObjectMeta_String(t *testing.T) {
+	tests := []struct {
+		name     string
+		objMeta  descriptor.ObjectMeta
+		expected string
+	}{
+		{
+			name: "with name only",
+			objMeta: descriptor.ObjectMeta{
+				Name: "test-object",
+			},
+			expected: "test-object",
+		},
+		{
+			name: "with name and version",
+			objMeta: descriptor.ObjectMeta{
+				Name:    "test-object",
+				Version: "1.0.0",
+			},
+			expected: "test-object:1.0.0",
+		},
+		{
+			name: "with name, version and labels",
+			objMeta: descriptor.ObjectMeta{
+				Name:    "test-object",
+				Version: "1.0.0",
+				Labels: []descriptor.Label{
+					{Name: "type", Value: "library"},
+					{Name: "priority", Value: "high"},
+				},
+			},
+			expected: "test-object:1.0.0+labels([{type library false} {priority high false}])",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := tt.objMeta.String()
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
+
+func TestElementMeta_String(t *testing.T) {
+	// Setup
+	elemMeta := descriptor.ElementMeta{
+		ObjectMeta: descriptor.ObjectMeta{
+			Name:    "test-element",
+			Version: "2.0.0",
+			Labels: []descriptor.Label{
+				{Name: "type", Value: "backend"},
+			},
+		},
+		ExtraIdentity: runtime.Identity{
+			"namespace": "system",
+			"platform":  "linux",
+		},
+	}
+
+	// Test
+	result := elemMeta.String()
+
+	// Assert
+	assert.Contains(t, result, "test-element:2.0.0")
+	assert.Contains(t, result, "+labels([{type backend false}])")
+	assert.Contains(t, result, "+extraIdentity(map[")
+	assert.Contains(t, result, "namespace:system")
+	assert.Contains(t, result, "platform:linux")
+}
+
+func TestElementMeta_ToIdentity(t *testing.T) {
+	// Setup
+	elemMeta := descriptor.ElementMeta{
+		ObjectMeta: descriptor.ObjectMeta{
+			Name:    "test-element",
+			Version: "2.0.0",
+		},
+		ExtraIdentity: runtime.Identity{
+			"namespace": "system",
+		},
+	}
+
+	// Test
+	identity := elemMeta.ToIdentity()
+
+	// Assert
+	expected := runtime.Identity{
+		"name":      "test-element",
+		"version":   "2.0.0",
+		"namespace": "system",
+	}
+	assert.Equal(t, expected, identity)
+}
+
+func TestElementMeta_ToIdentity_Nil(t *testing.T) {
+	// Test
+	var elemMeta *descriptor.ElementMeta
+	identity := elemMeta.ToIdentity()
+
+	// Assert
+	assert.Nil(t, identity)
+}
+
+func TestComponentMeta_ToIdentity(t *testing.T) {
+	// Setup
+	compMeta := descriptor.ComponentMeta{
+		ObjectMeta: descriptor.ObjectMeta{
+			Name:    "test-component",
+			Version: "3.0.0",
+			Labels: []descriptor.Label{
+				{Name: "stage", Value: "dev"},
+			},
+		},
+	}
+
+	// Test
+	identity := compMeta.ToIdentity()
+
+	// Assert
+	expected := runtime.Identity{
+		"name":    "test-component",
+		"version": "3.0.0",
+	}
+	assert.Equal(t, expected, identity)
+}
+
+func TestComponentMeta_ToIdentity_Nil(t *testing.T) {
+	// Test
+	var compMeta *descriptor.ComponentMeta
+	identity := compMeta.ToIdentity()
+
+	// Assert
+	assert.Nil(t, identity)
+}
+
+func TestNewExcludeFromSignatureDigest(t *testing.T) {
+	// Test
+	digest := descriptor.NewExcludeFromSignatureDigest()
+
+	// Assert
+	assert.Equal(t, descriptor.NoDigest, digest.HashAlgorithm)
+	assert.Equal(t, descriptor.ExcludeFromSignature, digest.NormalisationAlgorithm)
+	assert.Equal(t, descriptor.NoDigest, digest.Value)
+}
+
+func TestTimestamp_MarshalJSON(t *testing.T) {
+	// Setup
+	ts := &descriptor.Timestamp{}
+	timeValue := time.Date(2023, 10, 15, 14, 30, 45, 123456789, time.UTC)
+	ts.Time = descriptor.NewTime(timeValue)
+
+	// Test
+	data, err := json.Marshal(ts)
+
+	// Assert
+	require.NoError(t, err)
+	assert.Equal(t, `"2023-10-15T14:30:45Z"`, string(data))
+}
+
+func TestTimestamp_UnmarshalJSON(t *testing.T) {
+	// Setup
+	ts := &descriptor.Timestamp{}
+	jsonData := []byte(`"2023-10-15T14:30:45Z"`)
+
+	// Test
+	err := json.Unmarshal(jsonData, ts)
+
+	// Assert
+	require.NoError(t, err)
+	expectedTime := time.Date(2023, 10, 15, 14, 30, 45, 0, time.UTC)
+	assert.Equal(t, expectedTime, ts.Time.Time)
+}
+
+func TestTimestamp_UnmarshalJSON_Null(t *testing.T) {
+	// Setup
+	ts := &descriptor.Timestamp{}
+	jsonData := []byte(`null`)
+
+	// Test
+	err := json.Unmarshal(jsonData, ts)
+
+	// Assert
+	require.NoError(t, err)
+	assert.Equal(t, time.Time{}, ts.Time.Time)
+}
+
+func TestTimestamp_UnmarshalJSON_InvalidFormat(t *testing.T) {
+	// Setup
+	ts := &descriptor.Timestamp{}
+	jsonData := []byte(`"2023-10-15"`)
+
+	// Test
+	err := json.Unmarshal(jsonData, ts)
+
+	// Assert
+	require.Error(t, err)
+}
+
+func TestTimestamp_MarshalJSON_InvalidYear(t *testing.T) {
+	// Setup
+	ts := &descriptor.Timestamp{}
+	timeValue := time.Date(10000, 1, 1, 0, 0, 0, 0, time.UTC)
+	ts.Time = descriptor.NewTime(timeValue)
+
+	// Test
+	_, err := json.Marshal(ts)
+
+	// Assert
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), "year outside of range")
+}
+
+func TestConstantValues(t *testing.T) {
+	// Test identity attribute constants
+	assert.Equal(t, "name", descriptor.IdentityAttributeName)
+	assert.Equal(t, "version", descriptor.IdentityAttributeVersion)
+
+	// Test digest constants
+	assert.Equal(t, "EXCLUDE-FROM-SIGNATURE", descriptor.ExcludeFromSignature)
+	assert.Equal(t, "NO-DIGEST", descriptor.NoDigest)
+
+	// Test resource relation constants
+	assert.Equal(t, descriptor.ResourceRelation("local"), descriptor.LocalRelation)
+	assert.Equal(t, descriptor.ResourceRelation("external"), descriptor.ExternalRelation)
+}
+
+func TestResource_Struct(t *testing.T) {
+	// Setup
+	resource := descriptor.Resource{
+		ElementMeta: descriptor.ElementMeta{
+			ObjectMeta: descriptor.ObjectMeta{
+				Name:    "test-resource",
+				Version: "1.0.0",
+			},
+		},
+		Type:     "ociImage",
+		Relation: descriptor.LocalRelation,
+		Access: &runtime.Raw{
+			Type: runtime.Type{
+				Name: "ociArtifact",
+			},
+			Data: []byte(`{"type":"ociArtifact","imageReference":"test/image:1.0"}`),
+		},
+		Digest: &descriptor.Digest{
+			HashAlgorithm:          "SHA-256",
+			NormalisationAlgorithm: "OciArtifactDigest",
+			Value:                  "abcdef1234567890",
+		},
+		Size: 1024,
+	}
+
+	// Test
+	jsonData, err := json.Marshal(resource)
+
+	// Assert
+	require.NoError(t, err)
+	assert.Contains(t, string(jsonData), `"name":"test-resource"`)
+	assert.Contains(t, string(jsonData), `"version":"1.0.0"`)
+	assert.Contains(t, string(jsonData), `"type":"ociImage"`)
+	assert.Contains(t, string(jsonData), `"relation":"local"`)
+	assert.Contains(t, string(jsonData), `"access":{"type":"ociArtifact","imageReference":"test/image:1.0"}`)
+	assert.Contains(t, string(jsonData), `"digest":{"hashAlgorithm":"SHA-256","normalisationAlgorithm":"OciArtifactDigest","value":"abcdef1234567890"}`)
+	assert.Contains(t, string(jsonData), `"size":1024`)
+}
+
+func TestSource_Struct(t *testing.T) {
+	// Setup
+	source := descriptor.Source{
+		ElementMeta: descriptor.ElementMeta{
+			ObjectMeta: descriptor.ObjectMeta{
+				Name:    "test-source",
+				Version: "1.0.0",
+			},
+		},
+		Type: "git",
+		Access: &runtime.Raw{
+			Type: runtime.Type{
+				Name: "gitHub",
+			},
+			Data: []byte(`{"type":"gitHub","repoUrl":"https://github.com/test/repo","commit":"abcdef"}`),
+		},
+	}
+
+	// Test
+	jsonData, err := json.Marshal(source)
+
+	// Assert
+	require.NoError(t, err)
+	assert.Contains(t, string(jsonData), `"name":"test-source"`)
+	assert.Contains(t, string(jsonData), `"version":"1.0.0"`)
+	assert.Contains(t, string(jsonData), `"type":"git"`)
+	assert.Contains(t, string(jsonData), `"access":{"type":"gitHub","repoUrl":"https://github.com/test/repo","commit":"abcdef"}`)
+}
+
+func TestReference_Struct(t *testing.T) {
+	// Setup
+	reference := descriptor.Reference{
+		ElementMeta: descriptor.ElementMeta{
+			ObjectMeta: descriptor.ObjectMeta{
+				Name:    "test-reference",
+				Version: "1.0.0",
+			},
+		},
+		Component: "referenced-component",
+		Digest: descriptor.Digest{
+			HashAlgorithm:          "SHA-256",
+			NormalisationAlgorithm: "JsonNormalization",
+			Value:                  "0123456789abcdef",
+		},
+	}
+
+	// Test
+	jsonData, err := json.Marshal(reference)
+
+	// Assert
+	require.NoError(t, err)
+	assert.Contains(t, string(jsonData), `"name":"test-reference"`)
+	assert.Contains(t, string(jsonData), `"version":"1.0.0"`)
+	assert.Contains(t, string(jsonData), `"componentName":"referenced-component"`)
+	assert.Contains(t, string(jsonData), `"digest":{"hashAlgorithm":"SHA-256","normalisationAlgorithm":"JsonNormalization","value":"0123456789abcdef"}`)
+}
+
+func TestSignature_Struct(t *testing.T) {
+	// Setup
+	signature := descriptor.Signature{
+		Name: "test-signature",
+		Digest: descriptor.Digest{
+			HashAlgorithm:          "SHA-256",
+			NormalisationAlgorithm: "JsonNormalization",
+			Value:                  "0123456789abcdef",
+		},
+		Signature: descriptor.SignatureInfo{
+			Algorithm: "RSASSA-PKCS1-V1_5",
+			Value:     "base64-encoded-signature-value",
+			MediaType: "application/vnd.ocm.signature.rsa",
+			Issuer:    "Test Issuer",
+		},
+	}
+
+	// Test
+	jsonData, err := json.Marshal(signature)
+
+	// Assert
+	require.NoError(t, err)
+	assert.Contains(t, string(jsonData), `"name":"test-signature"`)
+	assert.Contains(t, string(jsonData), `"digest":{"hashAlgorithm":"SHA-256","normalisationAlgorithm":"JsonNormalization","value":"0123456789abcdef"}`)
+	assert.Contains(t, string(jsonData), `"signature":{"algorithm":"RSASSA-PKCS1-V1_5","value":"base64-encoded-signature-value","mediaType":"application/vnd.ocm.signature.rsa","issuer":"Test Issuer"}`)
+}
+
+func TestComponentDeserialization(t *testing.T) {
+	jsonData := `{
+		"meta": {
+			"schemaVersion": "v2"
+		},
+		"component": {
+			"name": "example-component",
+			"version": "1.0.0",
+			"provider": "Example Provider",
+			"resources": [
+				{
+					"name": "example-resource",
+					"version": "1.0.0",
+					"type": "ociImage",
+					"relation": "local",
+					"access": {
+						"type": "ociArtifact",
+						"imageReference": "example/image:1.0.0"
+					}
+				}
+			],
+			"sources": [
+				{
+					"name": "example-source",
+					"version": "1.0.0",
+					"type": "git",
+					"access": {
+						"type": "github",
+						"repoUrl": "https://github.com/example/repo"
+					}
+				}
+			],
+			"componentReferences": [
+				{
+					"name": "example-reference",
+					"version": "1.0.0",
+					"componentName": "other-component"
+				}
+			]
+		}
+	}`
+
+	var desc descriptor.Descriptor
+	err := json.Unmarshal([]byte(jsonData), &desc)
+
+	require.NoError(t, err)
+	assert.Equal(t, "v2", desc.Meta.Version)
+	assert.Equal(t, "example-component", desc.Component.Name)
+	assert.Equal(t, "1.0.0", desc.Component.Version)
+	assert.Equal(t, "Example Provider", desc.Component.Provider)
+
+	// Check resources
+	require.Len(t, desc.Component.Resources, 1)
+	assert.Equal(t, "example-resource", desc.Component.Resources[0].Name)
+	assert.Equal(t, "1.0.0", desc.Component.Resources[0].Version)
+	assert.Equal(t, "ociImage", desc.Component.Resources[0].Type)
+	assert.Equal(t, descriptor.LocalRelation, desc.Component.Resources[0].Relation)
+
+	// Check sources
+	require.Len(t, desc.Component.Sources, 1)
+	assert.Equal(t, "example-source", desc.Component.Sources[0].Name)
+	assert.Equal(t, "1.0.0", desc.Component.Sources[0].Version)
+	assert.Equal(t, "git", desc.Component.Sources[0].Type)
+
+	// Check references
+	require.Len(t, desc.Component.References, 1)
+	assert.Equal(t, "example-reference", desc.Component.References[0].Name)
+	assert.Equal(t, "1.0.0", desc.Component.References[0].Version)
+	assert.Equal(t, "other-component", desc.Component.References[0].Component)
 }
