df9edb9536e1bedca285a8a9ca9204da79ffdfee
Jakob Möller <jakob.moeller@sap.com>
2025-04-14 11:36:08 +0200

    feat: DAG Library used by KRO adopted for OCM (#21)
    * feat: DAG Library used by KRO adopted for OCM
    
    We want to use the KRO Graph Modelling Feature for Dependency Modelling while transferring resources. For this we are copying and adapting KROs take on Resource Graphs with a few adjustments:
    
    1. We introduce generic capability to make the graphs work with anything in cmp.Ordered
    2. We introduce InDegree/OutDegree to be able to selectively traverse the graph, by processing a graph through the indegree/outdegree.
    
    * chore: fixup

---
+++ bindings/go/dag/dag.go
@@ -0,0 +1,328 @@
+// # Modified from https://github.com/kro-run/kro/blob/7e437f2fe159a1e1c59d8eefd2bfa55320df4489/pkg/graph/dag/dag.go under Apache 2.0 License
+//
+// Original License:
+//
+// Copyright 2025 The Kube Resource Orchestrator Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may
+// not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+//     http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
+// We would like to thank the authors of kro for their outstanding work on this code.
+
+package dag
+
+import (
+	"cmp"
+	"fmt"
+	"maps"
+	"slices"
+	"sort"
+	"strings"
+)
+
+var ErrSelfReference = fmt.Errorf("self-references are not allowed")
+
+// Vertex represents a node/vertex in a directed acyclic graph.
+type Vertex[T cmp.Ordered] struct {
+	// ID is a unique identifier for the node
+	ID T
+	// Attributes stores the attributes of the node, such as the component
+	// descriptor.
+	Attributes map[string]any
+	// Edges stores the IDs of the nodes that this node has an outgoing edge to.
+	// In kro, this would be the children of a resource.
+	Edges map[T]map[string]any
+}
+
+// DirectedAcyclicGraph represents a directed acyclic graph.
+type DirectedAcyclicGraph[T cmp.Ordered] struct {
+	// Vertices stores the nodes in the graph
+	Vertices  map[T]*Vertex[T]
+	OutDegree map[T]int // Out-degree of each vertex (number of outgoing edges)
+	InDegree  map[T]int // In-degree of each vertex (number of incoming edges)
+}
+
+// NewDirectedAcyclicGraph creates a new directed acyclic graph.
+func NewDirectedAcyclicGraph[T cmp.Ordered]() *DirectedAcyclicGraph[T] {
+	return &DirectedAcyclicGraph[T]{
+		Vertices:  make(map[T]*Vertex[T]),
+		OutDegree: make(map[T]int),
+		InDegree:  make(map[T]int),
+	}
+}
+
+func (d *DirectedAcyclicGraph[T]) Clone() *DirectedAcyclicGraph[T] {
+	return &DirectedAcyclicGraph[T]{
+		Vertices:  maps.Clone(d.Vertices),
+		OutDegree: maps.Clone(d.OutDegree),
+		InDegree:  maps.Clone(d.InDegree),
+	}
+}
+
+// AddVertex adds a new node to the graph.
+func (d *DirectedAcyclicGraph[T]) AddVertex(id T, attributes ...map[string]any) error {
+	if _, exists := d.Vertices[id]; exists {
+		return fmt.Errorf("node %v already exists", id)
+	}
+	d.Vertices[id] = &Vertex[T]{
+		ID:         id,
+		Attributes: make(map[string]any),
+		Edges:      make(map[T]map[string]any),
+	}
+
+	for _, attributes := range attributes {
+		maps.Copy(d.Vertices[id].Attributes, attributes)
+	}
+
+	d.OutDegree[id] = 0
+	d.InDegree[id] = 0
+	return nil
+}
+
+// DeleteVertex removes a node from the graph.
+func (d *DirectedAcyclicGraph[T]) DeleteVertex(id T) error {
+	if _, exists := d.Vertices[id]; !exists {
+		return fmt.Errorf("node %v does not exist", id)
+	}
+
+	// Remove all edges to this node
+	for _, node := range d.Vertices {
+		if _, exists := node.Edges[id]; exists {
+			// Decrement the in-degree of the node
+			d.InDegree[node.ID]--
+			// Remove the edge from the node
+			delete(node.Edges, id)
+		}
+	}
+
+	delete(d.Vertices, id)
+	delete(d.OutDegree, id)
+	delete(d.InDegree, id)
+
+	return nil
+}
+
+type CycleError struct {
+	Cycle []string
+}
+
+func (e *CycleError) Error() string {
+	return fmt.Sprintf("The current graph would create a cycle: %s", formatCycle(e.Cycle))
+}
+
+func formatCycle(cycle []string) string {
+	return strings.Join(cycle, " -> ")
+}
+
+// AddEdge adds a directed edge from one node to another.
+func (d *DirectedAcyclicGraph[T]) AddEdge(from, to T, attributes ...map[string]any) error {
+	fromNode, fromExists := d.Vertices[from]
+	_, toExists := d.Vertices[to]
+	if !fromExists {
+		return fmt.Errorf("node %v does not exist", from)
+	}
+	if !toExists {
+		return fmt.Errorf("node %v does not exist", to)
+	}
+	if from == to {
+		return ErrSelfReference
+	}
+
+	_, exists := fromNode.Edges[to]
+
+	if !exists {
+		// Only initialize the map if the edge was added
+		fromNode.Edges[to] = map[string]any{}
+		// Only increment the out-degree and in-degree if the edge was added
+		d.OutDegree[from]++
+		d.InDegree[to]++
+
+		// Check if the graph is still a DAG
+		hasCycle, cycle := d.HasCycle()
+		if hasCycle {
+			// Ehmmm, we have a cycle, let's remove the edge we just added
+			delete(fromNode.Edges, to)
+			d.OutDegree[from]--
+			d.InDegree[to]--
+
+			return fmt.Errorf("adding an edge from %v to %v would create a cycle: %w", fmt.Sprintf("%v", from), fmt.Sprintf("%v", to), &CycleError{
+				Cycle: cycle,
+			})
+		}
+	}
+
+	for _, attributes := range attributes {
+		maps.Copy(fromNode.Edges[to], attributes)
+	}
+
+	return nil
+}
+
+func (d *DirectedAcyclicGraph[T]) Roots() []T {
+	var roots []T
+	for node, degree := range d.InDegree {
+		if degree == 0 {
+			roots = append(roots, node)
+		}
+	}
+	return roots
+}
+
+func (d *DirectedAcyclicGraph[T]) TopologicalSort() ([]T, error) {
+	if cyclic, nodes := d.HasCycle(); cyclic {
+		return nil, &CycleError{
+			Cycle: nodes,
+		}
+	}
+
+	visited := make(map[T]bool)
+	var order []T
+
+	// Get a sorted list of all vertices
+	vertices := d.GetVertices()
+
+	var dfs func(T)
+	dfs = func(node T) {
+		visited[node] = true
+
+		// Sort the neighbors to ensure deterministic order
+		neighbors := make([]T, 0, len(d.Vertices[node].Edges))
+		for neighbor := range d.Vertices[node].Edges {
+			neighbors = append(neighbors, neighbor)
+		}
+		slices.Sort(neighbors)
+
+		for _, neighbor := range neighbors {
+			if !visited[neighbor] {
+				dfs(neighbor)
+			}
+		}
+		order = append(order, node)
+	}
+
+	// Visit nodes in a deterministic order
+	for _, node := range vertices {
+		if !visited[node] {
+			dfs(node)
+		}
+	}
+
+	return order, nil
+}
+
+// GetVertices returns the nodes in the graph in sorted alphabetical
+// order.
+func (d *DirectedAcyclicGraph[T]) GetVertices() []T {
+	nodes := make([]T, 0, len(d.Vertices))
+	for node := range d.Vertices {
+		nodes = append(nodes, node)
+	}
+
+	// Ensure deterministic order. This is important for TopologicalSort
+	// to return a deterministic result.
+	slices.Sort(nodes)
+	return nodes
+}
+
+// GetEdges returns the edges in the graph in sorted order...
+func (d *DirectedAcyclicGraph[T]) GetEdges() [][2]T {
+	var edges [][2]T
+	for from, node := range d.Vertices {
+		for to := range node.Edges {
+			edges = append(edges, [2]T{from, to})
+		}
+	}
+	sort.Slice(edges, func(i, j int) bool {
+		// Sort by from node first
+		if edges[i][0] == edges[j][0] {
+			return edges[i][1] < edges[j][1]
+		}
+		return edges[i][0] < edges[j][0]
+	})
+	return edges
+}
+
+func (d *DirectedAcyclicGraph[T]) HasCycle() (bool, []string) {
+	visited := make(map[T]bool)
+	recStack := make(map[T]bool)
+	var cyclePath []string
+
+	var dfs func(T) bool
+	dfs = func(node T) bool {
+		visited[node] = true
+		recStack[node] = true
+		cyclePath = append(cyclePath, fmt.Sprintf("%v", node))
+
+		for neighbor := range d.Vertices[node].Edges {
+			if !visited[neighbor] {
+				if dfs(neighbor) {
+					return true
+				}
+			} else if recStack[neighbor] {
+				// Found a cycle, add the closing node to complete the cycle
+				cyclePath = append(cyclePath, fmt.Sprintf("%v", neighbor))
+				return true
+			}
+		}
+
+		recStack[node] = false
+		cyclePath = cyclePath[:len(cyclePath)-1]
+		return false
+	}
+
+	for node := range d.Vertices {
+		if !visited[node] {
+			cyclePath = []string{}
+			if dfs(node) {
+				// Trim the cycle path to start from the repeated node
+				start := 0
+				for i, v := range cyclePath[:len(cyclePath)-1] {
+					if v == cyclePath[len(cyclePath)-1] {
+						start = i
+						break
+					}
+				}
+				return true, cyclePath[start:]
+			}
+		}
+	}
+
+	return false, nil
+}
+
+func (d *DirectedAcyclicGraph[T]) Contains(v T) (ok bool) {
+	_, ok = d.Vertices[v]
+	return
+}
+
+// Reverse converts Parent → Child to Child → Parent.
+// This is useful for traversing the graph in reverse order.
+func (d *DirectedAcyclicGraph[T]) Reverse() (*DirectedAcyclicGraph[T], error) {
+	reverse := NewDirectedAcyclicGraph[T]()
+
+	// Ensure all vertices exist in the new graph
+	for _, parent := range d.Vertices {
+		if err := reverse.AddVertex(parent.ID); err != nil {
+			return nil, err
+		}
+	}
+
+	// Reverse the edges: Child -> Parent instead of Parent -> Child
+	for _, parent := range d.Vertices {
+		for child := range parent.Edges {
+			if err := reverse.AddEdge(child, parent.ID); err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	return reverse, nil
+}
---
+++ bindings/go/dag/dag_test.go
@@ -0,0 +1,194 @@
+// # Modified from https://github.com/kro-run/kro/blob/7e437f2fe159a1e1c59d8eefd2bfa55320df4489/pkg/graph/dag/dag.go under Apache 2.0 License
+//
+// Original License:
+//
+// Copyright 2025 The Kube Resource Orchestrator Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may
+// not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+//     http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
+// We would like to thank the authors of kro for their outstanding work on this code.
+
+package dag
+
+import (
+	"fmt"
+	"maps"
+	"slices"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDAGAddNode(t *testing.T) {
+	r := require.New(t)
+	d := NewDirectedAcyclicGraph[string]()
+
+	r.NoError(d.AddVertex("A", map[string]any{"key": "1"}))
+	r.Error(d.AddVertex("A", map[string]any{"key": "2"}), "duplicate node ids are forbidden")
+
+	r.True(d.Contains("A"))
+	r.False(d.Contains("B"))
+
+	r.Lenf(d.Vertices, 1, "expected 1 node after rejection of the second, but got %d", len(d.Vertices))
+	r.Equal("A", d.Vertices["A"].ID, "expected node ID to be 'A', but got %s", d.Vertices["A"].ID)
+	r.Equal("1", d.Vertices["A"].Attributes["key"], "expected node attribute to be '1', but got %s", d.Vertices["A"].Attributes["key"])
+
+	r.NoError(d.AddVertex("B", map[string]any{"key": "2"}))
+	r.Lenf(d.Vertices, 2, "expected 2 nodes after adding 'B', but got %d", len(d.Vertices))
+	r.Equal("B", d.Vertices["B"].ID, "expected node ID to be 'B', but got %s", d.Vertices["B"].ID)
+	r.Equal("2", d.Vertices["B"].Attributes["key"], "expected node attribute to be '2', but got %s", d.Vertices["B"].Attributes["key"])
+
+	t.Run("roots", func(t *testing.T) {
+		r := require.New(t)
+		roots := d.Roots()
+		r.Len(roots, 2, "expected 2 roots, but got %d", len(d.Roots()))
+		r.EqualValues([]string{"A", "B"}, d.Roots(), "expected roots to be [A B], but got %v", d.Roots())
+	})
+
+	t.Run("degrees", func(t *testing.T) {
+		r := require.New(t)
+		r.Equal(d.OutDegree["A"], 0, "expected out-degree of A to be 0, but got %d", d.OutDegree["A"])
+		r.Equal(d.InDegree["A"], 0, "expected in-degree of A to be 0, but got %d", d.InDegree["A"])
+		r.Equal(d.OutDegree["B"], 0, "expected out-degree of B to be 0, but got %d", d.OutDegree["B"])
+		r.Equal(d.InDegree["B"], 0, "expected in-degree of B to be 0, but got %d", d.InDegree["B"])
+	})
+
+	t.Run("delete", func(t *testing.T) {
+		r := require.New(t)
+		r.NoError(d.DeleteVertex("A"))
+		r.Lenf(d.Vertices, 1, "expected 1 node after deleting 'A', but got %d", len(d.Vertices))
+		r.Equal("B", d.Vertices["B"].ID, "expected node ID to be 'B', but got %s", d.Vertices["B"].ID)
+		r.Error(d.DeleteVertex("A"), "expected error when deleting non-existent node 'A', but got nil")
+		_, outExists := d.OutDegree["A"]
+		r.False(outExists)
+		_, inExists := d.InDegree["A"]
+		r.False(inExists)
+	})
+}
+
+func TestDAGAddEdge(t *testing.T) {
+	r := require.New(t)
+	d := NewDirectedAcyclicGraph[string]()
+	r.NoError(d.AddVertex("A"))
+	r.NoError(d.AddVertex("B"))
+	r.NoError(d.AddEdge("A", "B", map[string]any{"key": "1"}))
+
+	t.Run("roots", func(t *testing.T) {
+		r := require.New(t)
+		roots := d.Roots()
+		r.Len(roots, 1, "expected 1 root (A), but got %d", len(d.Roots()))
+		r.EqualValues([]string{"A"}, d.Roots(), "expected roots to be [A], but got %v", d.Roots())
+	})
+
+	r.Len(d.Vertices["A"].Edges, 1, "expected 1 edge from A to B, but got %d", len(d.Vertices["A"].Edges))
+	r.EqualValues([]string{"B"}, slices.Collect(maps.Keys(d.Vertices["A"].Edges)), "expected edge ID to be 'B', but got %s", d.Vertices["A"].Edges)
+	r.Len(d.Vertices["B"].Edges, 0, "expected 0 edges from B to A, but got %d", len(d.Vertices["B"].Edges))
+
+	t.Run("degrees", func(t *testing.T) {
+		r.Equal(d.OutDegree["A"], 1, "expected out-degree of A to be 1, but got %d", d.OutDegree["A"])
+		r.Equal(d.InDegree["A"], 0, "expected in-degree of A to be 0, but got %d", d.InDegree["A"])
+
+		r.Equal(d.OutDegree["B"], 0, "expected out-degree of B to be 0, but got %d", d.OutDegree["B"])
+		r.Equal(d.InDegree["B"], 1, "expected in-degree of B to be 1, but got %d", d.InDegree["B"])
+	})
+
+	t.Run("reverse", func(t *testing.T) {
+		r := require.New(t)
+		d, err := d.Reverse()
+		r.NoError(err, "error reversing the graph")
+		r.Len(d.Vertices["A"].Edges, 0, "expected 0 edges from A to B, but got %d", len(d.Vertices["A"].Edges))
+		r.Len(d.Vertices["B"].Edges, 1, "expected 1 edge from B to A, but got %d", len(d.Vertices["B"].Edges))
+		r.EqualValues([]string{"A"}, slices.Collect(maps.Keys(d.Vertices["B"].Edges)), "expected edge ID to be 'A', but got %s", d.Vertices["B"].Edges)
+	})
+
+	t.Run("delete", func(t *testing.T) {
+		r := require.New(t)
+		r.NoError(d.DeleteVertex("A"))
+		r.Lenf(d.Vertices, 1, "expected 1 node after deleting 'A', but got %d", len(d.Vertices))
+		r.Equal("B", d.Vertices["B"].ID, "expected node ID to be 'B', but got %s", d.Vertices["B"].ID)
+		r.Error(d.DeleteVertex("A"), "expected error when deleting non-existent node 'A', but got nil")
+		_, outExists := d.OutDegree["A"]
+		r.False(outExists)
+		_, inExists := d.InDegree["A"]
+		r.False(inExists)
+	})
+}
+
+func TestDAGHasCycle(t *testing.T) {
+	r := require.New(t)
+	d := NewDirectedAcyclicGraph[string]()
+	r.NoError(d.AddVertex("A"))
+	r.NoError(d.AddVertex("B"))
+	r.NoError(d.AddVertex("C"))
+
+	r.NoError(d.AddEdge("A", "B"))
+	r.NoError(d.AddEdge("B", "C"))
+
+	cyclic, _ := d.HasCycle()
+	r.False(cyclic, "DAG incorrectly reported a cycle")
+
+	r.Error(d.AddEdge("C", "A"), "expected error when creating a cycle, but got nil")
+
+	// pointless to test for the cycle here, so we need to emulate one
+	// by artificially adding a cycle.
+	d.Vertices["C"].Edges["A"] = map[string]any{}
+	cyclic, _ = d.HasCycle()
+	r.Truef(cyclic, "DAG incorrectly reported no cycle")
+
+	_, err := d.TopologicalSort()
+	r.Errorf(err, "expected error when sorting a cyclic graph, but got nil")
+	r.IsType(&CycleError{}, err, "expected CycleError, but got %T", err)
+	r.EqualValues(err.(*CycleError).Cycle, []string{"A", "B", "C", "A"}, "expected cycle to be [A B C], but got %v", err.(*CycleError).Cycle)
+}
+
+func TestDAGTopologicalSort(t *testing.T) {
+	grid := []struct {
+		Nodes string
+		Edges string
+		Want  string
+	}{
+		{Nodes: "A,B", Want: "A,B"},
+		{Nodes: "A,B", Edges: "A->B", Want: "B,A"},
+		{Nodes: "A,B", Edges: "B->A", Want: "A,B"},
+		{Nodes: "A,B,C,D,E,F", Edges: "", Want: "A,B,C,D,E,F"},
+		{Nodes: "A,B,C,D,E,F", Edges: "C->D", Want: "A,B,C,D,E,F"},
+		{Nodes: "A,B,C,D,E,F", Edges: "D->C", Want: "A,B,D,E,F,C"},
+		{Nodes: "A,B,C,D,E,F", Edges: "F->A,F->B,B->A", Want: "C,D,E,F,B,A"},
+		{Nodes: "A,B,C,D,E,F", Edges: "B->A,C->A,D->B,D->C,F->E,A->E", Want: "D,F,B,C,A,E"},
+	}
+
+	for i, g := range grid {
+		t.Run(fmt.Sprintf("[%d] nodes=%s,edges=%s", i, g.Nodes, g.Edges), func(t *testing.T) {
+			r := require.New(t)
+			d := NewDirectedAcyclicGraph[string]()
+			for _, node := range strings.Split(g.Nodes, ",") {
+				r.NoError(d.AddVertex(node))
+			}
+
+			if g.Edges != "" {
+				for _, edge := range strings.Split(g.Edges, ",") {
+					tokens := strings.SplitN(edge, "->", 2)
+					r.NoError(d.AddEdge(tokens[0], tokens[1]))
+				}
+			}
+
+			order, err := d.TopologicalSort()
+			r.NoError(err, "error sorting the graph")
+
+			expected := strings.Split(g.Want, ",")
+			r.ElementsMatch(order, expected, "unexpected result from TopologicalSort for nodes=%q edges=%q, got %q, want %q", g.Nodes, g.Edges, order, expected)
+
+			// checkValidTopologicalOrder(t, d, order)
+		})
+	}
+}
