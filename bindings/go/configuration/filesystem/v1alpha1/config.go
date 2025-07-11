7cbbb713b74ef4d43cfeb0b70f67ca2a92abfa6b
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2025-07-11 17:38:04 +0200

    feat: add central configuration types for logging and filesystem (#389)
    * feat: add central configuration types for logging and filesystem
    
    Signed-off-by: Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
    
    On-behalf-of: @SAP gergely.brautigam@sap.com
    
    * use os.tempDir as default and remove logging part
    
    Signed-off-by: Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
    
    On-behalf-of: @SAP gergely.brautigam@sap.com

---
+++ bindings/go/configuration/filesystem/v1alpha1/config.go
@@ -0,0 +1,100 @@
+package v1alpha1
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	v1 "ocm.software/open-component-model/bindings/go/configuration/v1"
+	"ocm.software/open-component-model/bindings/go/runtime"
+)
+
+const (
+	// ConfigType defines the type identifier for credential configurations
+	ConfigType = "filesystem.config.ocm.software"
+)
+
+var scheme = runtime.NewScheme()
+
+func init() {
+	scheme.MustRegisterWithAlias(&Config{}, runtime.NewVersionedType(ConfigType, Version))
+}
+
+// Config represents the top-level configuration for the plugin manager.
+//
+// +k8s:deepcopy-gen:interfaces=ocm.software/open-component-model/bindings/go/runtime.Typed
+// +k8s:deepcopy-gen=true
+// +ocm:typegen=true
+type Config struct {
+	Type runtime.Type `json:"-"`
+	// TempFolder defines places where plugins and other functionalities can put ephemeral files under.
+	// If not defined, os.TempDir is used as a default.
+	TempFolder string `json:"tempFolder,omitempty"`
+}
+
+type Duration time.Duration
+
+func (d Duration) MarshalJSON() ([]byte, error) {
+	return json.Marshal(time.Duration(d).String())
+}
+
+func (d *Duration) UnmarshalJSON(b []byte) error {
+	var v interface{}
+
+	return json.Unmarshal(b, &v)
+}
+
+// LookupConfig creates a new filesystem configuration from a central V1 config.
+func LookupConfig(cfg *v1.Config) (*Config, error) {
+	var merged *Config
+	if cfg != nil {
+		cfg, err := v1.Filter(cfg, &v1.FilterOptions{
+			ConfigTypes: []runtime.Type{
+				runtime.NewVersionedType(ConfigType, Version),
+				runtime.NewUnversionedType(ConfigType),
+			},
+		})
+		if err != nil {
+			return nil, fmt.Errorf("failed to filter config: %w", err)
+		}
+		cfgs := make([]*Config, 0, len(cfg.Configurations))
+		for _, entry := range cfg.Configurations {
+			var config Config
+			if err := scheme.Convert(entry, &config); err != nil {
+				return nil, fmt.Errorf("failed to decode credential config: %w", err)
+			}
+			cfgs = append(cfgs, &config)
+		}
+		merged = Merge(cfgs...)
+		if merged == nil {
+			merged = &Config{}
+		}
+	} else {
+		merged = new(Config)
+	}
+
+	if len(merged.TempFolder) == 0 {
+		merged.TempFolder = os.TempDir()
+	}
+
+	return merged, nil
+}
+
+// Merge merges the provided configs into a single config.
+func Merge(configs ...*Config) *Config {
+	if len(configs) == 0 {
+		return nil
+	}
+
+	merged := new(Config)
+	_, _ = scheme.DefaultType(merged)
+
+	for _, config := range configs {
+		if config.TempFolder != merged.TempFolder {
+			merged.TempFolder = config.TempFolder
+		}
+	}
+
+	return merged
+}
---
+++ bindings/go/configuration/filesystem/v1alpha1/doc.go
@@ -0,0 +1,2 @@
+// Package v1alpha1 any config that can influence the filesystem.
+package v1alpha1
