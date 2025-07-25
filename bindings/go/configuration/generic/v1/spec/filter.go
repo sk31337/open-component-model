7a70534411d42599162f430b55265fc2371759a3
Fabian Burth <fabian.burth@sap.com>
2025-07-25 15:00:18 +0200

    feat: filter for type (#457)
    <!-- markdownlint-disable MD041 -->
    #### What this PR does / why we need it
    A common use case when working with ocm configuration is wanting to
    filter for a specific type of configuration that you want to work with.
    This PR introduces a function which makes this a lot easier.
    
    #### Which issue(s) this PR fixes
    <!--
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    
    ---------
    
    Signed-off-by: Fabian Burth <fabian.burth@sap.com>
    Co-authored-by: Jakob Möller <jakob.moeller@sap.com>

--- bindings/go/configuration/generic/v1/spec/filter.go
+++ bindings/go/configuration/generic/v1/spec/filter.go
@@ -1,6 +1,7 @@
 package spec
 
 import (
+	"fmt"
 	"slices"
 
 	"ocm.software/open-component-model/bindings/go/runtime"
@@ -25,4 +26,35 @@ func Filter(config *Config, options *FilterOptions) (*Config, error) {
 	}
 
 	return filtered, nil
+}
+
+// FilterForType filters the configuration for a specific configuration type T
+// and returns a slice of typed configurations.
+func FilterForType[T runtime.Typed](scheme *runtime.Scheme, config *Config) ([]T, error) {
+	typ, err := scheme.TypeForPrototype(*new(T))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create get type for prototype of type %T: %w", typ, err)
+	}
+
+	types := append(scheme.GetTypes()[typ], typ) //nolint:gocritic // appendAssign to new variable should be safe here
+
+	filtered, err := Filter(config, &FilterOptions{
+		ConfigTypes: types,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to filter for types %v: %w", types, err)
+	}
+	typedConfigs := make([]T, 0, len(filtered.Configurations))
+	for _, cfg := range filtered.Configurations {
+		obj, err := scheme.NewObject(typ)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create object for type %s: %w", typ, err)
+		}
+		if err := scheme.Convert(cfg, obj); err != nil {
+			return nil, fmt.Errorf("failed to convert config of type %s to object of type %s: %w", cfg.GetType(), typ, err)
+		}
+		typedConfigs = append(typedConfigs, obj.(T))
+	}
+
+	return typedConfigs, nil
 }
--- bindings/go/configuration/generic/v1/spec/filter_test.go
+++ bindings/go/configuration/generic/v1/spec/filter_test.go
@@ -4,6 +4,7 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/stretchr/testify/require"
 	"ocm.software/open-component-model/bindings/go/runtime"
 )
 
@@ -107,6 +108,127 @@ func TestFilter(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Filter() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+type testConfig struct {
+	Type  runtime.Type `json:"type"`
+	Value string       `json:"value"`
+}
+
+func (c *testConfig) GetType() runtime.Type {
+	return c.Type
+}
+
+func (c *testConfig) SetType(typ runtime.Type) {
+	c.Type = typ
+}
+
+func (c *testConfig) DeepCopyTyped() runtime.Typed {
+	return &testConfig{
+		Type:  c.Type,
+		Value: c.Value,
+	}
+}
+
+func TestFilterForType(t *testing.T) {
+	r := require.New(t)
+
+	schemeWithTestConfig := runtime.NewScheme()
+	r.NoError(schemeWithTestConfig.RegisterWithAlias(&testConfig{},
+		runtime.NewUnversionedType("test"),
+		runtime.NewVersionedType("test", "v1")))
+
+	type args struct {
+		scheme *runtime.Scheme
+		config *Config
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []*testConfig
+		wantErr bool
+	}{
+		{
+			name: "passing config with registered type should succeed",
+			args: args{
+				scheme: schemeWithTestConfig,
+				config: &Config{
+					Type: runtime.Type{
+						Version: "v1",
+						Name:    "test",
+					},
+					Configurations: []*runtime.Raw{
+						{
+							Type: runtime.Type{
+								Version: "v1",
+								Name:    "test",
+							},
+							Data: []byte(`{"type": "test/v1", "value": "value1"}`),
+						},
+					},
+				},
+			},
+			want: []*testConfig{
+				{
+					Type: runtime.Type{
+						Version: "v1",
+						Name:    "test",
+					},
+					Value: "value1",
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "passing config with unregistered type should return error",
+			args: args{
+				scheme: runtime.NewScheme(),
+				config: &Config{
+					Type: runtime.Type{
+						Version: "v1",
+						Name:    "test",
+					},
+					Configurations: []*runtime.Raw{
+						{
+							Type: runtime.Type{
+								Version: "v1",
+								Name:    "test",
+							},
+						},
+					},
+				},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "passing config with empty configurations should return empty slice",
+			args: args{
+				scheme: schemeWithTestConfig,
+				config: &Config{
+					Type: runtime.Type{
+						Version: "v1",
+						Name:    "test",
+					},
+					Configurations: []*runtime.Raw{},
+				},
+			},
+			want:    []*testConfig{},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := FilterForType[*testConfig](tt.args.scheme, tt.args.config)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FilterForType() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("FilterForType() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
