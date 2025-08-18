b5fe1ed48d9e3946387d6bd9ace3d00ec6cc6f8b
Gergely Brautigam <gergely.brautigam@sap.com>
2025-08-18 15:53:02 +0200

    fix: integration test failure (#584)
    #### What this PR does / why we need it
    
    #### Which issue(s) this PR fixes
    <!--
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    
    ---------
    
    Signed-off-by: Gergely Brautigam <182850+Skarlso@users.noreply.github.com>

--- bindings/go/oci/integration/integration_test.go
+++ bindings/go/oci/integration/integration_test.go
@@ -907,16 +907,19 @@ func getUserAndPasswordWithGitHubCLIAndJQ(t *testing.T) (string, string) {
 
 	out, err := exec.CommandContext(t.Context(), "sh", "-c", fmt.Sprintf("%s api user", gh)).CombinedOutput()
 	if err != nil {
+		t.Logf("gh CLI output: %s", out)
 		t.Errorf("gh CLI for user failed: %v", err)
 	}
 	structured := map[string]interface{}{}
 	if err := json.Unmarshal(out, &structured); err != nil {
+		t.Logf("gh CLI output: %s", out)
 		t.Errorf("gh failed to parse output: %v", err)
 	}
 	user := structured["login"].(string)
 
 	pw := exec.CommandContext(t.Context(), "sh", "-c", fmt.Sprintf("%s auth token", gh))
 	if out, err = pw.CombinedOutput(); err != nil {
+		t.Logf("gh auth token output: %s", out)
 		t.Errorf("gh CLI for password failed: %v", err)
 	}
 	password := strings.TrimSpace(string(out))
